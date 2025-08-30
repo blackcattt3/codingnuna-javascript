397a014410e01fc004e0fe7d36ccccad939c6e88
Jaehee Roh <snowcat3b@gmail.com>
2025-08-30 18:35:11 +0900

    test1

--- codingnuna_JVS/to_do_list/main.js
+++ codingnuna_JVS/to_do_list/main.js
@@ -3,11 +3,20 @@ let addBtn = document.querySelector("#add-btn");
 let taskList = [];
 let checkBtn = document.querySelector("#check-btn");
 let deleteBtn = document.querySelector("#delete-btn");
-
+let tabs = document.querySelectorAll(".task-tabs div");
+let mode = 'all';
+let filterList = [];
+// let notDoneList = [];
+// let doneList = [];
 addBtn.addEventListener("click", addTask);
 // checkBtn.addEventListener("click", function(){
 //     isComplete = true;
 // })
+console.log(tabs);
+
+for(let i=1;i<tabs.length;i++){
+    tabs[i].addEventListener("click", function(event){filter(event)});
+}
 
 function addTask(){
     // 객체 사용!
@@ -17,7 +26,7 @@ function addTask(){
         isComplete: false
     }
     taskList.push(task);
-    console.log(taskList);
+    // console.log(taskList);
     render();
     taskInput.value = "";
     taskInput.focus();
@@ -25,27 +34,46 @@ function addTask(){
 
 // UI 요소 표시 함수
 function render(){
+    // 내가 선택한 탭에 따라서
+    // 리스트를 달리 보여준다.
+    list = [];
+    if(mode=='all'){
+        // all : taskList
+        list = taskList;
+    } else if(mode=='not-done' || mode=='done'){
+        // done, not-done : filterList
+        list = filterList;
+    }
     let resultHTML = '';
     let taskBoard = document.querySelector("#task-board");
-    for(let i=0;i<taskList.length;i++){
-        if(taskList[i].isComplete == true){
+    for(let i=0;i<list.length;i++){
+        if(list[i].isComplete == true){
             resultHTML += `<div class="list">
-            <div class="task-done">${taskList[i].taskContent}</div>
+            <div class="task-done">${list[i].taskContent}</div>
             <div>
-                <button onclick="toggleComplete('${taskList[i].id}')" id="check-btn">check</button>
-                <button id="delete-btn">delete</button>
+                <button onclick="toggleComplete('${list[i].id}')" id="check-btn">
+                    <i class="fa-solid fa-arrow-rotate-left"></i>
+                </button>
+                <button onclick="deleteItem('${list[i].id}')" id="delete-btn">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
             </div>
         </div>`;
         } else{
             resultHTML += `<div class="list">
-            <div>${taskList[i].taskContent}</div>
+            <div>${list[i].taskContent}</div>
             <div>
-                <button onclick="toggleComplete('${taskList[i].id}')" id="check-btn">check</button>
-                <button id="delete-btn">delete</button>
+                <button onclick="toggleComplete('${list[i].id}')" id="check-btn">
+                    <i class="fa-solid fa-check"></i>
+                </button>
+                <button onclick="deleteItem('${list[i].id}')" id="delete-btn">
+                    <i class="fa-solid fa-trash-can"></i>
+                </button>
             </div>
         </div>`;
         }
     }
+    // moveToDone();
     taskBoard.innerHTML  = `${resultHTML}`;
 }
 
@@ -60,7 +88,7 @@ function toggleComplete(id){
     //     })
     // }
     // isComplete = true;
-    console.log(id);
+    // console.log(id);
     for(let i=0;i<taskList.length;i++){
         if(id == taskList[i].id){
             // console.log(taskList[i].taskContent);
@@ -70,9 +98,65 @@ function toggleComplete(id){
         }
     }
     render();
-    console.log(taskList);
+    // console.log(taskList);
 }
 
 function randomIDGenerate(){
     return Math.random().toString(36).substr(2, 9);
+}
+
+function deleteItem(id){
+    for(let i=0;i<taskList.length;i++){
+        if(id==taskList[i].id){
+            taskList.splice(i, 1);
+            break;
+        }
+    }
+    render();
+}
+
+// function moveToDone(){
+//     let notDoneList = [];
+//     let doneList = [];
+//     for(let i=0;i<taskList.length;i++){
+//         if(taskList[i].isComplete == true){
+//             doneList.push(taskList[i]);
+//         } else if(taskList[i].isComplete == false){
+//             notDoneList.push(taskList[i]);
+//         }
+//     }
+//     console.log(doneList);
+//     console.log(notDoneList);
+// }
+
+
+
+// ctrl + d
+// target !! this는 안되네
+function filter(event){
+    console.log(event.target.id);
+    mode = event.target.id      // let 없이 전역변수로 선언!
+    filterList = [];
+    if(mode === "all"){
+        // 전체 목록 보여주기
+    } else if(mode === "not-done"){
+        // not done 목록 보여주기
+        // taskList[i].isComplete == false
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete === false){
+                filterList.push(taskList[i]);
+            }
+        }
+        console.log(filterList);
+    } else if(mode === "done"){
+        // done 목록 보여주기
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete === true){
+                filterList.push(taskList[i]);
+            }
+        }
+        console.log(filterList);
+    }
+    render();
+    // console.log(filterList);
 }
