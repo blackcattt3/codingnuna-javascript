5fd40d94266e3d5258b458afa96e8939927983ef
Jaehee Roh <snowcat3b@gmail.com>
2025-09-10 00:22:19 +0900

    test1

---
+++ test1/main.js
@@ -0,0 +1,18 @@
+const buyTV = () => {
+    greeting("형아 가전")
+}
+
+const buyAC = () =>{
+    greeting("형아 가전")
+}
+
+const buyCellphone = () =>{
+    greeting("형아 가전")
+}
+
+const greeting = (name)=>{
+    console.log("TV 구매 감사드립니다.")
+    console.log(`${name}에 와주셔서 감사하고 항상 건강하세요!`)
+}
+
+buyTV();
